4b2075c3c040ecf97fa664a43b1cf5c20c0b25f2
vaish-sv22 <svvaishnavi03@gmail.com>
2026-07-20 12:55:51 +0530

    Add files via upload

---
+++ scripts-shell/minikube.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo yum install -y docker
+sudo systemctl start docker.service
+sudo systemctl enable docker.service
+
+curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/
+
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+chmod +x minikube
+sudo mv minikube /usr/local/bin/
+sudo yum install conntrack -y
+sudo  minikube start --force
+sudo kubectl get pods -A 
+sudo kubectl get nodes
